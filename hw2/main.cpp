f41217049a941e70cb5a246fb075c792d376e794
ihsueh <peter881999@gmail.com>
2020-04-01 00:28:32 +0800

    done!

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -12,22 +12,22 @@ AnalogIn Ain(A0);
 BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
 char table[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
 
-int sample = 1000;
+int sample = 400;
 int i;
 int s[3];
-float ADCdata[5000];
+float ADCdata[800];
 void play(int n);
 
 int main(){
   redLED = 1;
   greenLED = 0;
-  for (i = 0; i < 5*sample; i++){
+  for (i = 0; i < 2*sample; i++){
     //Aout = Ain;
     ADCdata[i] = Ain;
     wait(1./sample);
   }
   int q = 0;
-  for (i = 0; i < 5*sample; i++){
+  for (i = 0; i < 2*sample; i++){
     if(ADCdata[i-1]>ADCdata[i] && ADCdata[i]<=ADCdata[i+1]){
       //pc.printf("%d   ", i);
       //pc.printf("%1.5f\r\n", ADCdata[i]);
@@ -38,8 +38,8 @@ int main(){
     //wait(0.1);
   }
   //q = q*0.99;
-  pc.printf("%d\r\n", q/5);
-  q = q/5;
+  //pc.printf("%d\r\n", q/10);
+  q = q/2;
   int i;
   for(i=0; q!=0; i++){
     s[i] = q%10;
